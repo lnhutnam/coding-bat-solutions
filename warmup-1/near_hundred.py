603956f7e95d9c012ff7dc06b3b2a40e7d3aac89
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-10 20:27:53 +0700

    Update near_hundred.py

---
+++ warmup-1/near_hundred.py
@@ -0,0 +1,28 @@
+"""
+Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+
+near_hundred(93) → True
+near_hundred(90) → True
+near_hundred(89) → False
+"""
+
+import unittest
+
+
+def near_hundred(n):
+    pass
+
+
+class Test(unittest.TestCase):
+    def test_case_00(self):
+        self.assertEqual(near_hundred(93), True)
+
+    def test_case_01(self):
+        self.assertEqual(near_hundred(90), True)
+
+    def test_case_02(self):
+        self.assertEqual(near_hundred(89), False)
+
+if __name__ == "__main__":
+    unittest.main()
