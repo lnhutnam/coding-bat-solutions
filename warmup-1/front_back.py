20171542a294b9c61a69b2d391b3ff4474f98562
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-10 22:58:47 +0700

    Update front_back.py

---
+++ warmup-1/front_back.py
@@ -0,0 +1,20 @@
+"""
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+"""
+
+import unittest
+
+
+def front_back(str):
+    pass
+
+class Test(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
