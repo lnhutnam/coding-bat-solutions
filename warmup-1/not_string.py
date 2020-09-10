677ff54303344852fe58119b91ea15d2f68a0375
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-10 22:58:51 +0700

    Update not_string.py

---
+++ warmup-1/not_string.py
@@ -0,0 +1,20 @@
+"""
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+
+not_string('candy') → 'not candy'
+not_string('x') → 'not x'
+not_string('not bad') → 'not bad'
+"""
+
+import unittest
+
+
+def not_string(str):
+    pass
+
+class Test(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
