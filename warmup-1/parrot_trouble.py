8df97df4b0e664e98a4145dfbdf9b16c6f612148
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-10 20:10:49 +0700

    Update parrot_trouble.py

--- warmup-1/parrot_trouble.py
+++ warmup-1/parrot_trouble.py
@@ -11,7 +11,7 @@ import unittest
 
 
 def parrot_trouble(talking, hour):
-    pass
+    return (False, (hour < 7 or hour > 20))[talking]
 
 
 class MyTest(unittest.TestCase):
@@ -23,6 +23,24 @@ class MyTest(unittest.TestCase):
 
     def test_case_03(self):
         self.assertEqual(parrot_trouble(False, 6), False)
+
+    def test_case_04(self):
+        self.assertEqual(parrot_trouble(False, 21), False)
+
+    def test_case_05(self):
+        self.assertEqual(parrot_trouble(False, 20), False)
+
+    def test_case_06(self):
+        self.assertEqual(parrot_trouble(True, 23), True)
+
+    def test_case_07(self):
+        self.assertEqual(parrot_trouble(False, 23), False)
+
+    def test_case_08(self):
+        self.assertEqual(parrot_trouble(True, 20), False)
+
+    def test_case_09(self):
+        self.assertEqual(parrot_trouble(False, 12), False)
 
 
 if __name__ == "__main__":
