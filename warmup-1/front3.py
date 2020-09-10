a9ca5e06638b591727b9d6130399721b7cf7cea5
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-10 22:58:45 +0700

    Update front3.py

---
+++ warmup-1/front3.py
@@ -0,0 +1,21 @@
+"""
+
+Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+front3('Java') → 'JavJavJav'
+front3('Chocolate') → 'ChoChoCho'
+front3('abc') → 'abcabcabc'
+"""
+
+import unittest
+
+
+def front3(str):
+    pass
+
+class Test(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
